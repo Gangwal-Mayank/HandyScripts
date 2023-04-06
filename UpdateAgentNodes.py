0e04d5e1ef8b6f4052baedad9eacd7c00b45f4f6
Mayank Gangwal <106341450+Gangwal-Mayank@users.noreply.github.com>
2023-04-06 14:02:39 -0400

    Update UpdateAgentNodes.py

--- UpdateAgentNodes.py
+++ UpdateAgentNodes.py
@@ -6,9 +6,9 @@ print ("Please provide the details below:")
 jenkins_url = input('Enter your controller url: ')
 username = input('Enter your UserName: ')
 password = getpass.getpass('Enter your Password: ')
-JenkinsHome = input('Abosulte path for your jenkins home (example - /var/lib/jenkins/): ')
-ElementToUpdate = input('Enter the Element you want to update: ')
-NewValue = input('Enter the new value: ')
+jenkinsHome = input('Abosulte path for your jenkins home (example - /var/lib/jenkins/): ')
+elementToUpdate = input('Enter the Element you want to update: ')
+newValue = input('Enter the new value: ')
 jenkins = Jenkins(jenkins_url, username=username, password=password)
 # Get the list of all nodes
 nodes = jenkins.nodes
@@ -18,8 +18,8 @@ for node_name, node_obj in nodes.items():
         # Parse the XML file
         tree = ET.parse(f'/var/lib/jenkins/nodes/{node_name}/config.xml')
         root = tree.getroot()
-        element_to_update = root.find(f'.//{ElementToUpdate}')
-        element_to_update.text = NewValue
-        tree.write (f'{JenkinsHome}nodes/{node_name}/config.xml', xml_declaration=True, encoding='UTF-8', method="xml")
-        print (f'Node - {node_name} element {ElementToUpdate} is now updated to {NewValue}')
+        element_to_update = root.find(f'.//{elementToUpdate}')
+        element_to_update.text = newValue
+        tree.write (f'{jenkinsHome}nodes/{node_name}/config.xml', xml_declaration=True, encoding='UTF-8', method="xml")
+        print (f'Node - {node_name} element {elementToUpdate} is now updated to {newValue}')
 print ('Changes will take effect after either a restart or reload configuration from disk')
